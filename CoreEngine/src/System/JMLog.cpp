6c5496a68047ba80bd9aa13d98a7904978f45b7d
Joe Morton <jmorton06@live.co.uk>
2019-01-11 22:11:44 +0000

    Update JMLog.cpp

--- CoreEngine/src/System/JMLog.cpp
+++ CoreEngine/src/System/JMLog.cpp
@@ -11,10 +11,10 @@ namespace jm
     void JMLog::Init()
     {
         spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("JM");
+        s_CoreLogger = spdlog::stdout_color_mt("JM ");
         s_CoreLogger->set_level(spdlog::level::trace);
         
-        s_ClientLogger = spdlog::stdout_color_mt("APP");
+        s_ClientLogger = spdlog::stdout_color_mt("APP ");
         s_ClientLogger->set_level(spdlog::level::trace);
     }
 }
