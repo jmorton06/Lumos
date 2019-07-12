8d27e754234a5afe74fb80fb72c12bcc849e06dc
jmorton06 <jmorton06@live.co.uk>
2019-07-12 20:22:06 +0100

    vma vulkan depth textures

--- Lumos/src/Platform/Vulkan/VKTextureDepth.cpp
+++ Lumos/src/Platform/Vulkan/VKTextureDepth.cpp
@@ -20,11 +20,11 @@ namespace Lumos
 				VKDevice::Instance()->GetDevice().destroySampler(m_TextureSampler);
 	
 			VKDevice::Instance()->GetDevice().destroyImageView(m_TextureImageView);
-//#ifdef USE_VMA_ALLOCATOR
- //           vmaDestroyImage(VKDevice::Instance()->GetAllocator(), m_TextureImage, m_Allocation);
-//#else
+#ifdef USE_VMA_ALLOCATOR
+            vmaDestroyImage(VKDevice::Instance()->GetAllocator(), m_TextureImage, m_Allocation);
+#else
             VKDevice::Instance()->GetDevice().destroyImage(m_TextureImage);
-//#endif
+#endif
 			VKDevice::Instance()->GetDevice().freeMemory(m_TextureImageMemory);
 		}
 
@@ -96,19 +96,18 @@ namespace Lumos
 			imageInfo.samples = vk::SampleCountFlagBits::e1;
 			imageInfo.sharingMode = vk::SharingMode::eExclusive;
 
-//#ifdef USE_VMA_ALLOCATOR
-//            VmaAllocationCreateInfo allocInfovma;
-//            allocInfovma.flags = 0;
-//            allocInfovma.usage = VMA_MEMORY_USAGE_GPU_ONLY;
-//            allocInfovma.requiredFlags = 0;
-//            allocInfovma.preferredFlags = 0;
-//            allocInfovma.memoryTypeBits = 0;
-//            allocInfovma.pool = nullptr;
-//            allocInfovma.pUserData = nullptr;
-//            vmaCreateImage(VKDevice::Instance()->GetAllocator(), reinterpret_cast<VkImageCreateInfo*>(&imageInfo), &allocInfovma, reinterpret_cast<VkImage*>(&image), &m_Allocation, nullptr);
-//#else
+#ifdef USE_VMA_ALLOCATOR
+            VmaAllocationCreateInfo allocInfovma;
+            allocInfovma.flags = 0;
+            allocInfovma.usage = VMA_MEMORY_USAGE_GPU_ONLY;
+            allocInfovma.requiredFlags = 0;
+            allocInfovma.preferredFlags = 0;
+            allocInfovma.memoryTypeBits = 0;
+            allocInfovma.pool = nullptr;
+            allocInfovma.pUserData = nullptr;
+            vmaCreateImage(VKDevice::Instance()->GetAllocator(), reinterpret_cast<VkImageCreateInfo*>(&imageInfo), &allocInfovma, reinterpret_cast<VkImage*>(&image), &m_Allocation, nullptr);
+#else
             image = VKDevice::Instance()->GetDevice().createImage(imageInfo);
-//#endif
 
 			vk::MemoryRequirements memRequirements;
 			VKDevice::Instance()->GetDevice().getImageMemoryRequirements(image, &memRequirements);
@@ -119,6 +118,7 @@ namespace Lumos
 
 			imageMemory = VKDevice::Instance()->GetDevice().allocateMemory(allocInfo);
 			VKDevice::Instance()->GetDevice().bindImageMemory(image, imageMemory, 0);
+#endif
 		}
 
 		vk::ImageView VKTextureDepth::CreateImageView(vk::Image image, vk::Format format, vk::ImageAspectFlags aspectFlags)
