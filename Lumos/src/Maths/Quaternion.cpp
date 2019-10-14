d946450f969114e8d3252408ea5f520700694628
Joe Morton <jmorton06@live.co.uk>
2019-10-14 21:57:14 +0100

    Quaternion progress

--- Lumos/src/Maths/Quaternion.cpp
+++ Lumos/src/Maths/Quaternion.cpp
@@ -448,37 +448,37 @@ namespace Lumos
 
 		void Quaternion::RotatePointByQuaternion(const Quaternion &quat, Vector3 &point)
 		{
-//#ifdef LUMOS_SSEQUAT
-//			__m128 q = quat.mmvalue;
-//				q = _mm_shuffle_ps(q, q, _MM_SHUFFLE(0, 3, 2, 1));
-//#ifdef LUMOS_SSEVEC3
-//			__m128 v = point.m_Value;
-//#else
-//			__m128 v = _mm_set_ps(0.f, rhs.z, rhs.y, rhs.x);
-//#endif
-//			const __m128 W = _mm_shuffle_ps(q, q, _MM_SHUFFLE(3, 3, 3, 3));
-//			const __m128 a_yzx = _mm_shuffle_ps(q, q, _MM_SHUFFLE(3, 0, 2, 1));
-//			__m128 x = _mm_mul_ps(q, _mm_shuffle_ps(v, v, _MM_SHUFFLE(3, 0, 2, 1)));
-//			__m128 qxv = _mm_sub_ps(x, _mm_mul_ps(a_yzx, v));
-//			__m128 Wv = _mm_mul_ps(W, v);
-//			__m128 s = _mm_add_ps(qxv, _mm_shuffle_ps(Wv, Wv, _MM_SHUFFLE(3, 1, 0, 2)));
-//			__m128 qs = _mm_mul_ps(q, s);
-//			__m128 y = _mm_shuffle_ps(qs, qs, _MM_SHUFFLE(3, 1, 0, 2));
-//			s = _mm_sub_ps(_mm_mul_ps(a_yzx, s), y);
-//			s = _mm_add_ps(s, s);
-//			s = _mm_add_ps(s, v);
-//
-//			Vector3 pos(
-//				_mm_cvtss_f32(s),
-//				_mm_cvtss_f32(_mm_shuffle_ps(s, s, _MM_SHUFFLE(1, 1, 1, 1))),
-//				_mm_cvtss_f32(_mm_movehl_ps(s, s)));
-//#else
+#ifdef LUMOS_SSEQUAT
+			__m128 q = quat.mmvalue;
+				q = _mm_shuffle_ps(q, q, _MM_SHUFFLE(0, 3, 2, 1));
+#ifdef LUMOS_SSEVEC3
+			__m128 v = point.m_Value;
+#else
+			__m128 v = _mm_set_ps(0.f, rhs.z, rhs.y, rhs.x);
+#endif
+			const __m128 W = _mm_shuffle_ps(q, q, _MM_SHUFFLE(3, 3, 3, 3));
+			const __m128 a_yzx = _mm_shuffle_ps(q, q, _MM_SHUFFLE(3, 0, 2, 1));
+			__m128 x = _mm_mul_ps(q, _mm_shuffle_ps(v, v, _MM_SHUFFLE(3, 0, 2, 1)));
+			__m128 qxv = _mm_sub_ps(x, _mm_mul_ps(a_yzx, v));
+			__m128 Wv = _mm_mul_ps(W, v);
+			__m128 s = _mm_add_ps(qxv, _mm_shuffle_ps(Wv, Wv, _MM_SHUFFLE(3, 1, 0, 2)));
+			__m128 qs = _mm_mul_ps(q, s);
+			__m128 y = _mm_shuffle_ps(qs, qs, _MM_SHUFFLE(3, 1, 0, 2));
+			s = _mm_sub_ps(_mm_mul_ps(a_yzx, s), y);
+			s = _mm_add_ps(s, s);
+			s = _mm_add_ps(s, v);
+
+			Vector3 pos(
+				_mm_cvtss_f32(s),
+				_mm_cvtss_f32(_mm_shuffle_ps(s, s, _MM_SHUFFLE(1, 1, 1, 1))),
+				_mm_cvtss_f32(_mm_movehl_ps(s, s)));
+#else
 			Vector3 qVec(quat.x, quat.y, quat.z);
 			Vector3 cross1(qVec.Cross(point));
 			Vector3 cross2(qVec.Cross(cross1));
 
 			Vector3 pos = (point + ((cross1 * quat.w + cross2) * 2.0f));
-//#endif
+#endif
 			point.SetX(pos.x);
 			point.SetY(pos.y);
 			point.SetZ(pos.z);
@@ -642,19 +642,19 @@ namespace Lumos
 
 		Quaternion Quaternion::operator*(const Quaternion& q) const
 		{
-//#ifdef LUMOS_SSEQUAT
-//			__m128 q1 = mmvalue;
-//			__m128 q2 = q.mmvalue;
-//			q2 = _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(0, 3, 2, 1));
-//			const __m128 signy = _mm_castsi128_ps(_mm_set_epi32((int)0x80000000UL, (int)0x80000000UL, 0, 0));
-//			const __m128 signx = _mm_shuffle_ps(signy, signy, _MM_SHUFFLE(2, 0, 2, 0));
-//			const __m128 signz = _mm_shuffle_ps(signy, signy, _MM_SHUFFLE(3, 0, 0, 3));
-//			__m128 out = _mm_mul_ps(_mm_shuffle_ps(q1, q1, _MM_SHUFFLE(1, 1, 1, 1)), _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(0, 1, 2, 3)));
-//			out = _mm_add_ps(_mm_mul_ps(_mm_xor_ps(signy, _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(2, 2, 2, 2))), _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(1, 0, 3, 2))), _mm_xor_ps(signx, out));
-//			out = _mm_add_ps(_mm_mul_ps(_mm_xor_ps(signz, _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(3, 3, 3, 3))), _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(2, 3, 0, 1))), out);
-//			out = _mm_add_ps(_mm_mul_ps(_mm_shuffle_ps(q1, q1, _MM_SHUFFLE(0, 0, 0, 0)), q2), out);
-//			return Quaternion(_mm_shuffle_ps(out, out, _MM_SHUFFLE(2, 1, 0, 3)));
-//#else
+#ifdef LUMOS_SSEQUAT
+			__m128 q1 = mmvalue;
+			__m128 q2 = q.mmvalue;
+			q2 = _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(0, 3, 2, 1));
+			const __m128 signy = _mm_castsi128_ps(_mm_set_epi32((int)0x80000000UL, (int)0x80000000UL, 0, 0));
+			const __m128 signx = _mm_shuffle_ps(signy, signy, _MM_SHUFFLE(2, 0, 2, 0));
+			const __m128 signz = _mm_shuffle_ps(signy, signy, _MM_SHUFFLE(3, 0, 0, 3));
+			__m128 out = _mm_mul_ps(_mm_shuffle_ps(q1, q1, _MM_SHUFFLE(1, 1, 1, 1)), _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(0, 1, 2, 3)));
+			out = _mm_add_ps(_mm_mul_ps(_mm_xor_ps(signy, _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(2, 2, 2, 2))), _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(1, 0, 3, 2))), _mm_xor_ps(signx, out));
+			out = _mm_add_ps(_mm_mul_ps(_mm_xor_ps(signz, _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(3, 3, 3, 3))), _mm_shuffle_ps(q2, q2, _MM_SHUFFLE(2, 3, 0, 1))), out);
+			out = _mm_add_ps(_mm_mul_ps(_mm_shuffle_ps(q1, q1, _MM_SHUFFLE(0, 0, 0, 0)), q2), out);
+			return Quaternion(_mm_shuffle_ps(out, out, _MM_SHUFFLE(2, 1, 0, 3)));
+#else
 			Quaternion ans;
 
 			ans.w = (w * q.w) - (x * q.x) - (y * q.y) - (z * q.z);
@@ -663,7 +663,7 @@ namespace Lumos
 			ans.z = (z * q.w) + (w * q.z) + (x * q.y) - (y * q.x);
 
 			return ans;
-//#endif
+#endif
 		}
 
 		Quaternion Quaternion::operator*(const Vector3& v) const
